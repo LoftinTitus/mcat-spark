fcb2ad2ffcaa02647b164348dea814d5753bc19e
Ty Loftin <tyloftin4@gmail.com>
2025-12-14 17:44:22 -0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/mcat-spark/",
+  base: mode === "development" ? "/" : "/mcat-spark/",
   server: {
     host: "::",
     port: 8080,
